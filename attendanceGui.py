dcaeefa6d7294b39d79a011143f0ce44c551f187
vuwestin <36426709+vuwestin@users.noreply.github.com>
2019-08-15 17:50:19 -0700

    Add files via upload

--- attendanceGui.py
+++ attendanceGui.py
@@ -1,6 +1,192 @@
-from attendanceTaker import Student,attendanceTaker
-from tkinter import *
+from attendanceTaker import Student, classDatabase
+import tkinter as tk
 
-class attendanceGui:
-	def __init__(self):
-		self.window = Tk(className = " Attendance Taker")
+class attendanceGui(tk.Tk):
+
+	def __init__(self, *args, **kwargs):
+
+		tk.Tk.__init__(self, *args, **kwargs)
+		self.classDb = classDatabase("courseName", "leaderName", "dayAndTimeOfSession", "numSessions")
+		#self.classDb.readDatabase("attendance.txt")
+		self.currentWeek = 0
+		self.currentDay = 0
+
+		self.container = tk.Frame(self)
+		self.title("Attendance Taker V.1")
+		self.container.pack(side = "top", fill = "both", expand = True)
+		self.container.grid_rowconfigure(0, weight = 1)
+		self.container.grid_columnconfigure(0, weight = 1)
+
+		self.frames = {}
+		self.setupFrames()
+		#assign widgets to the grid
+
+		self.show_frame(StartPage)
+
+	def setupFrames(self):
+		for F in (StartPage, addStudentPage,attPage):
+
+			frame = F(self.container,self)
+
+			self.frames[F] = frame
+
+			frame.grid(row = 0, column= 0, sticky = "nsew")
+
+	def show_frame(self, controller):
+		self.updateClassDb("attendance.txt")
+		frame = self.frames[controller]
+		frame.tkraise()
+		# self.window = Tk()
+		# self.window.title("title yeah baby")
+		# self.window.geometry("350x200")
+		# self.panel = Frame(self.window)
+		# self.panel.pack()
+		# self.window.mainloop()	
+		#call mainloop in main function. duh
+
+	def setDayAndWeek(self, week, day):
+		week = int(week)
+		day = int(day)
+		if (week > 0 and week < 16) and (week > 0 and week < 3):
+			self.currentWeek = str(week)
+			self.currentDay = str(day)
+			self.show_frame(attPage)
+		#make attPage
+
+	def addStud(self, name, email, prof, sectionNum):
+		self.classDb.addStudent(Student(name, email, prof, sectionNum))
+		self.classDb.saveDatabase()
+		self.show_frame(StartPage)
+		#added students not showing in attpage
+
+	def saveClassDb(self, checkBoxList):
+		presentList = []
+		for x in checkBoxList:
+			#presentList is a list of tk.IntVar()
+			if x.get():
+				presentList.append(1)
+			else:
+				presentList.append(0)
+		for x in range(0, len(presentList)):
+			self.classDb.dataBase[x].addDay(int(self.currentWeek),int(self.currentDay), presentList[x])
+
+		self.classDb.saveDatabase()
+		self.show_frame(StartPage)
+
+	def updateClassDb(self, filename):
+		self.classDb.readDatabase(filename)
+		print("update")
+		# container = tk.Frame(self)
+		# container.pack(side = "top", fill = "both", expand = True)
+		# container.grid_rowconfigure(0, weight = 1)
+		# container.grid_columnconfigure(0, weight = 1)
+		self.frames[attPage] = attPage(self.container,self)
+		self.frames[attPage].grid(row = 0, column= 0, sticky = "nsew")
+
+class StartPage(tk.Frame):
+
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text = "Attendance Taker V 1.0")
+		label.grid(row = 0, column = 0)
+
+		weekLabel = tk.Label(self, text = "Week")
+		weekLabel.grid(row = 1, column = 0)
+
+		entry1 = tk.Entry(self,width = 5)
+		entry1.grid(row = 1, column = 1)
+
+		dayLabel = tk.Label(self, text = "Day (1 or 2)")
+		dayLabel.grid(row = 2, column = 0)
+
+		dayEntry = tk.Entry(self, width = 5)
+		dayEntry.grid(row = 2, column = 1)
+
+		button1 = tk.Button(self, text = "Continue", command = lambda: controller.setDayAndWeek(entry1.get(), dayEntry.get()))
+		button1.grid(row = 3, column = 2)
+
+		#add an addstudent button
+		addButton = tk.Button(self,text = "Add Student", command = lambda: controller.show_frame(addStudentPage))
+		addButton.grid(row = 3, column = 0)
+
+		#button1.grid_forget()
+		#grid_forget removes stuff
+
+class addStudentPage(tk.Frame):
+
+	def __init__(self,parent,controller):
+		tk.Frame.__init__(self, parent)
+
+		addLabel = tk.Label(self, text = "Enter the name of the student to add")
+		firstNameLabel = tk.Label(self, text = "First Name")
+		lastNameLabel = tk.Label(self, text = "Last Name")
+		emailLabel = tk.Label(self, text = "Email (without @csu.fullerton.edu)")
+		profLabel = tk.Label(self, text = "Professor's Last Name")
+		sectionNumLabel = tk.Label(self, text = "Class Section Number")
+
+		firstNameEntry = tk.Entry(self, width = 10)
+		lastNameEntry = tk.Entry(self, width = 10)
+		emailEntry = tk.Entry(self, width = 15)
+		profEntry = tk.Entry(self, width = 10)
+		sectionNumEntry = tk.Entry(self, width = 5)
+		
+
+		backButton = tk.Button(self, text = "Back", command = lambda: controller.show_frame(StartPage))
+		addButton = tk.Button(self, text = "Add", command = lambda: controller.addStud(lastNameEntry.get() + "," + firstNameEntry.get(), emailEntry.get(), profEntry.get(), sectionNumEntry.get()))
+
+		addLabel.grid(row = 0, column = 0)
+		firstNameLabel.grid(row = 1, column = 0)
+		lastNameLabel.grid(row = 2, column = 0)
+		emailLabel.grid(row = 3, column = 0)
+		profLabel.grid(row = 4, column = 0)
+		sectionNumLabel.grid(row = 5, column = 0)
+
+		firstNameEntry.grid(row = 1, column = 1)
+		lastNameEntry.grid(row = 2, column = 1)
+		emailEntry.grid(row = 3, column = 1)
+		profEntry.grid(row = 4, column = 1)
+		sectionNumEntry.grid(row = 5, column = 1)
+
+		backButton.grid(row = 6, column = 0)
+		addButton.grid(row = 6, column = 1)
+
+class attPage(tk.Frame):
+
+	def __init__(self,parent,controller):
+		#parent is a Frame, controller is an attendanceGui
+		tk.Frame.__init__(self, parent)
+		controller.classDb.readDatabase("attendance.txt")
+		self.studentCounter = 0
+		self.checkBoxArray = []
+		for student in controller.classDb.dataBase:
+			studLabel = tk.Label(self, text = (student.name))
+			studLabel.grid(row = self.studentCounter, column = 0)
+
+
+			self.checkBoxArray.append(tk.IntVar())
+
+			hereCheckButton = tk.Checkbutton(self,text = "Here", variable = self.checkBoxArray[self.studentCounter])
+			hereCheckButton.grid(row = self.studentCounter, column = 1)
+
+			#self.checkBoxArray.append(hereCheckButton)
+			self.studentCounter = self.studentCounter + 1
+
+		#do this
+		saveButton = tk.Button(self, text = "Save", command = lambda: controller.saveClassDb(self.checkBoxArray))
+		backButton = tk.Button(self, text = "Back", command = lambda: controller.show_frame(StartPage))
+		backButton.grid(row = self.studentCounter, column = 0)
+		saveButton.grid(row = self.studentCounter, column = 3)
+			#REDO THIS SO THAT ITS ONLY ONE CHECKBOX NEXT TO THE NAME FOR HERE
+			#OR NOT
+			#FIND OUT HOW TO TELL WHICH CHECKBOX GOT TICKED
+
+
+def main():
+	p = attendanceGui()
+	p.mainloop()
+	# rock = {}
+	# rock[attendanceGui] = 3
+	# print(rock[attendanceGui])
+
+if __name__ == "__main__":
+	main()
--- attendanceTaker.py
+++ attendanceTaker.py
@@ -1,17 +1,18 @@
 from tkinter import *
+from os import path
 
 class Student:
-	def __init__(self,firstName, lastName, email, prof, sectionNum):
-		self.firstName = firstName
-		self.lastName = lastName
+	def __init__(self,name, email, prof, sectionNum):
+		#self.firstName = firstName
+		self.name = name
 		self.email = email
 		self.prof = prof
 		self.sectionNum = sectionNum
 		self.daysPresent = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 		#30 sessions total
 
-	def addDay(self, week,day):
-		self.daysPresent[((week-1) * 2 - 1) + day] = 1
+	def addDay(self, week,day, val):
+		self.daysPresent[((week-1) * 2 - 1) + day] = val
 
 class classDatabase:
 	def __init__(self, courseName, leaderName, dayAndTimeOfSession, numSessions):
@@ -20,6 +21,7 @@ class classDatabase:
 		self.dayAndTimeOfSession = dayAndTimeOfSession
 		self.numSessions = numSessions
 		self.dataBase = []
+		#dataBase is a list of Students
 
 	def addStudent(self, student):
 		self.dataBase.append(student)
@@ -34,58 +36,64 @@ class classDatabase:
 			print(x.daysPresent)
 
 
-	def deleteStudent(self, studentName):
+	def deleteStudent(self, name):
 		for x in self.dataBase:
-			if x.studentName == studentName:
+			if (x.name == name):
 				self.dataBase.remove(x)
 
 	def saveDatabase(self):
 		f = open("attendance.txt", "w+")
 		for x in self.dataBase:
-			writeStr = x.firstName + "," + x.lastName + "	" + x.email + "	" + x.prof + "	" + x.sectionNum + "	" + str(len(x.daysPresent)) + "	" + str(len(x.daysPresent)) + "	"
+			writeStr = x.name + "	" + x.email + "	" + x.prof + "	" + x.sectionNum + "	" + str(len(x.daysPresent)) + "	" + str(len(x.daysPresent)) + "	"
 			for j in x.daysPresent:
 				writeStr = writeStr + str(j) + ","
 			writeStr = str(writeStr) + "\n"
 			f.write(str(writeStr))
 		f.close()
+		print("saves")
 
 	def readDatabase(self, fileName):
-		f = open(fileName, "r")
-		lines = f.readlines()
-		for x in lines:
-			studentList = x.split("	") #split by tabs
-			newStud = Student(studentList[0], studentList[1], studentList[2],
-				studentList[3], studentList[4])
-			daysList = studentList[6].split(",") #split by commas
-			for i in range(0,30):
-				newStud.daysPresent[i] = int(daysList[i])
-			self.dataBase.append(newStud)
+		if path.exists(fileName):
+			f = open(fileName, "r")
+			lines = f.readlines()
+			if len(lines) > 0:
+				self.dataBase = [] #clear database
+				for x in lines:
+					studentList = x.split("	") #split by tabs
+					newStud = Student(studentList[0], studentList[1],
+						studentList[2], studentList[3])
+					daysList = studentList[6].split(",") #split by commas
+					for i in range(0,30):
+						newStud.daysPresent[i] = int(daysList[i])
+					self.dataBase.append(newStud)
+			else:
+				self.database = []
+		else:
+			self.database = []
 
 			
 
 
 
-def main():
-	db = classDatabase("dummy", "dummy1", "dummy2", "dummy3")
-	# w = Student("Westin", "Vu", "vuwee", "pee", "poo")
-	# #db.addStudent(w)
-	# w.addDay(1,2)
-	# db.readDatabase("attendance.txt")
-	# db.printAllStudents()
-	m = Tk(className = ' Attendance Taker')
-	panel = Frame(m)
-	panel.pack()
-	button1 = Button(panel, text = "yeeyeeyee", bg = "red", fg = "red")
-	#label1 = Label(panel, bg = "green", fg = "black")
-	#label1.pack()
-	weekEntry = Entry(panel, width = 5)
-	weekEntry.pack()
-	button1.pack()
+#def main():
+	# db = classDatabase("dummy", "dummy1", "dummy2", "dummy3")
+	# # w = Student("Westin", "Vu", "vuwee", "pee", "poo")
+	# # #db.addStudent(w)
+	# # w.addDay(1,2)
+	# # db.readDatabase("attendance.txt")
+	# # db.printAllStudents()
+	# m = Tk(className = ' Attendance Taker')
+	# panel = Frame(m)
+	# panel.pack()
+	# button1 = Button(panel, text = "yeeyeeyee", bg = "red", fg = "red")
+	# #label1 = Label(panel, bg = "green", fg = "black")
+	# #label1.pack()
+	# weekEntry = Entry(panel, width = 5)
+	# weekEntry.pack()
+	# button1.pack()
 
-	#m.title('Attendance Taker')
-	m.mainloop()
-
-
+	# #m.title('Attendance Taker')
+	# m.mainloop()
 
 
 
@@ -93,5 +101,9 @@ def main():
 
 
 
-if __name__ == "__main__":
-	main()
+
+
+# if __name__ == "__main__":
+# 	main()
+
+
